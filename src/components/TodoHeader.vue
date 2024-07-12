b75662ca67dce2e2b5d975df7a44db795c7b2cc5
Armel K. Dovenon <dove.armel@gmail.com>
2024-07-12 15:47:03 +0100

    TodoHeader créé

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <RouterLink>
+      <h1>Todos</h1>
+    </RouterLink>
+
+    <input
+      class="new-todo"
+      type="text"
+      placeholder="Entrer une tâche"
+      autofocus
+      autocomplete="off"
+    />
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
