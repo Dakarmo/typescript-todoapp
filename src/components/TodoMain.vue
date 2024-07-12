607a91320e5734fdb0dd64c639de7b5889420475
Armel K. Dovenon <dove.armel@gmail.com>
2024-07-12 16:49:25 +0100

    TodoMain créé avec le composant TodoItem

---
+++ src/components/TodoMain.vue
@@ -0,0 +1,29 @@
+<template>
+    <main class="main">
+        <!-- <ul class="todo-list">
+            <li :class="{competed: todo.complete}" v-for="todo in taches" :key="todo.id">
+                <div class="view">
+                    <input type="checkbox" class="toggle ">
+                    <label for="">{{ todo.title }}</label>
+                    <button class="destroy"></button>
+                </div>
+            </li>
+        </ul> -->
+        <ul class="todo-list">
+            <TodoItem v-for="todo in taches" :key="todo.id" :todo="todo" />
+        </ul>
+    </main>
+</template>
+
+<script setup lang="ts">
+import type {Todo} from '@/@types';
+import TodoItem from '@/components/TodoItem.vue'
+
+// const props = defineProps(['taches']);
+
+const props = defineProps<{
+    taches: Todo[]
+}>();
+</script>
+
+<style scoped></style>
